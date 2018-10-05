88b8cb853065cae763b351ba9d0a347e7e06383c
akhil-bp <akhilbp94@gmail.com>
2018-10-05 17:38:04 +0530

    first commit

---
+++ webrtc-web-master/step-01/js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// On this codelab, you will be streaming only video (video: true).
+const mediaStreamConstraints = {
+  video: true,
+};
+
+// Video element where stream will be placed.
+const localVideo = document.querySelector('video');
+
+// Local stream that will be reproduced on the video.
+let localStream;
+
+// Handles success by adding the MediaStream to the video element.
+function gotLocalMediaStream(mediaStream) {
+  localStream = mediaStream;
+  localVideo.srcObject = mediaStream;
+  console.log(mediaStream)//me
+}
+
+// Handles error by logging a message to the console with the error message.
+function handleLocalMediaStreamError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+// Initializes media stream.
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+  .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+//getUserMedia() call, the browser requests permission from the user to access their camera
+//The constraints argument allows you to specify what media to get. In this example, video only, since audio is disabled by default:
+//If getUserMedia() is successful, the video stream from the webcam is set as the source of the video element:
